308b88d8cdfd94235068d1ee6237926740d50d11
Phil Mike Jones <philmikejones@gmail.com>
2016-06-15 15:43:24 +0100

    need to add wales in; eng joining well

--- data-raw/1-tidy-data.R
+++ data-raw/1-tidy-data.R
@@ -1,3 +1,4 @@
+# LADs ====
 car <- readr::read_csv("extdata/lad_car.csv")
 car <- car[-nrow(car), ]
 car <- dplyr::select(car, GEOGRAPHY_CODE, GEOGRAPHY_NAME, CELL_NAME, OBS_VALUE)
@@ -5,8 +6,9 @@ car <- tidyr::spread(car, CELL_NAME, OBS_VALUE)
 car[["pc_car"]] <- car[["No cars or vans in household"]] /
   car[["All categories: Car or van availability"]]
 car[["pc_car"]] <- car[["pc_car"]] * 100
-car <- dplyr::select(car, -3, -4)
-colnames(car) <- c("geo_code", "geo_name", "pc_car")
+car$z_car <- scale(car$pc_car, scale = TRUE, center = TRUE)
+car <- dplyr::select(car, -3, -4, -5)
+colnames(car) <- c("geo_code", "geo_name", "z_car")
 
 ppr <- readr::read_csv("extdata/lad_ppr.csv")
 ppr <- ppr[-nrow(ppr), ]
@@ -18,8 +20,9 @@ ppr[["pc_ppr"]] <- ppr[["Over 1.0 and up to 1.5 persons per room"]] +
 ppr[["pc_ppr"]] <- ppr[["pc_ppr"]] /
   ppr[["All categories: Number of persons per room in household"]]
 ppr[["pc_ppr"]] <- ppr[["pc_ppr"]] * 100
-ppr <- dplyr::select(ppr, -3, -4, -5)
-colnames(ppr) <- c("geo_code", "geo_name", "pc_ppr")
+ppr$z_ppr <- scale(ppr$pc_ppr, scale = TRUE, center = TRUE)
+ppr <- dplyr::select(ppr, -3, -4, -5, -6)
+colnames(ppr) <- c("geo_code", "geo_name", "z_ppr")
 
 ten <- readr::read_csv("extdata/lad_ten.csv")
 ten <- ten[-nrow(ten), ]
@@ -27,8 +30,9 @@ ten <- dplyr::select(ten, GEOGRAPHY_CODE, GEOGRAPHY_NAME, CELL_NAME, OBS_VALUE)
 ten <- tidyr::spread(ten, CELL_NAME, OBS_VALUE)
 ten[["pc_ten"]] <- ten[["Owned"]] / ten[["All households"]]
 ten[["pc_ten"]] <- ten[["pc_ten"]] * 100
-ten <- dplyr::select(ten, -3, -4)
-colnames(ten) <- c("geo_code", "geo_name", "pc_ten")
+ten$z_ten <- scale(ten$pc_ten, scale = TRUE, center = TRUE)
+ten <- dplyr::select(ten, -3, -4, -5)
+colnames(ten) <- c("geo_code", "geo_name", "z_ten")
 
 eau <- readr::read_csv("extdata/lad_eau.csv")
 eau <- eau[-nrow(eau), ]
@@ -37,8 +41,16 @@ eau <- tidyr::spread(eau, CELL_NAME, OBS_VALUE)
 eau[["pc_eau"]] <- eau[["Economically active: Unemployed"]] /
   eau[["Economically active: Total"]]
 eau[["pc_eau"]] <- eau[["pc_eau"]] * 100
-eau <- dplyr::select(eau, -3, -4)
-colnames(eau) <- c("geo_code", "geo_name", "pc_eau")
+eau$z_eau <- scale(eau$pc_eau, scale = TRUE, center = TRUE)
+eau <- dplyr::select(eau, -3, -4, -5)
+colnames(eau) <- c("geo_code", "geo_name", "z_eau")
+
+lad_townsend <- dplyr::left_join(car, ppr, by = c("geo_code", "geo_name"))
+lad_townsend <- dplyr::left_join(lad_townsend, ten,
+                                 by = c("geo_code", "geo_name"))
+lad_townsend <- dplyr::left_join(lad_townsend, eau,
+                                 by = c("geo_code", "geo_name"))
+rm(car, ppr, ten, eau)
 
 lad <- rgdal::readOGR(dsn = "extdata/lad", "england_lad_2011_gen")
 for (i in seq_along(colnames(lad@data))) {
@@ -46,11 +58,14 @@ for (i in seq_along(colnames(lad@data))) {
 }
 rm(i)
 
-no_match <- dplyr::anti_join(lad@data, car, by = c("label" = "geo_code"))
+no_match <- dplyr::anti_join(lad@data, lad_townsend,
+                             by = c("label" = "geo_code"))
 for (i in no_match[["name"]]) {
-  car$geo_code[car$geo_name == i] <- lad@data$label[lad@data$name == i]
-  ppr$geo_code[ppr$geo_name == i] <- lad@data$label[lad@data$name == i]
-  ten$geo_code[ten$geo_name == i] <- lad@data$label[lad@data$name == i]
-  eau$geo_code[eau$geo_name == i] <- lad@data$label[lad@data$name == i]
+  lad_townsend$geo_code[lad_townsend$geo_name == i] <-
+    lad@data$label[lad@data$name == i]
 }
 rm(i, no_match)
+
+lad@data <- dplyr::left_join(lad@data, lad_townsend,
+                             by = c("label" = "geo_code"))
+stop("Need to add Wales in somehow")
