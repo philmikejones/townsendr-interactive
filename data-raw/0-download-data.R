853c06ac9b102275c7f7731b1f638ab545ca3d86
Phil Mike Jones <philmikejones@gmail.com>
2016-06-15 11:44:15 +0100

    update packages packrat

--- data-raw/0-download-data.R
+++ data-raw/0-download-data.R
@@ -1,23 +1,23 @@
-# Base URL
-url_1 <- "https://www.nomisweb.co.uk/api/v01/dataset/"
-url_2 <- ".data.csv?date=latest&geography="
-url_3 <- "&rural_urban=0&cell=1...5&measures=20100&signature=NPK-0c73734c0f725c979cee3a:0xaabfb6be3b4d4f4a1253f7b9aaca60e457728be7"
+# LADs ====
+car <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/NM_621_1.data.csv?",
+              "date=latest&geography=TYPE464&rural_urban=0&cell=1...5&",
+              "measures=20100&signature=NPK-0c73734c0f725c979cee3a:",
+              "0xaabfb6be3b4d4f4a1253f7b9aaca60e457728be7")
+ppr <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/NM_541_1.data.csv?",
+              "date=latest&geography=TYPE464&rural_urban=0&cell=1...5&",
+              "measures=20100&signature=NPK-0c73734c0f725c979cee3a:",
+              "0xaabfb6be3b4d4f4a1253f7b9aaca60e457728be7")
+ten <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/NM_537_1.data.csv?",
+              "date=latest&geography=TYPE464&rural_urban=0&cell=1...5&",
+              "measures=20100&signature=NPK-0c73734c0f725c979cee3a:",
+              "0xaabfb6be3b4d4f4a1253f7b9aaca60e457728be7")
+eau <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/NM_556_1.data.csv?",
+              "date=latest&geography=TYPE464&rural_urban=0&cell=1...5&",
+              "measures=20100&signature=NPK-0c73734c0f725c979cee3a:",
+              "0xaabfb6be3b4d4f4a1253f7b9aaca60e457728be7")
 
-# Variable
-car   <- "NM_621_1"
-ppr   <- "NM_541_1"
-ten   <- "NM_537_1"
-une   <- "NM_556_1"
+downloader::download(car, destfile = "extdata/lad_car.csv")
 
-# Geography
-msoa  <- "1245708289...1245715489"
-lad   <- "TYPE464"
-
-
-
-download.file(paste0(url_1, car, url_2, lad, url_3),
-              destfile = "extdata/car-msoa.csv")
-readr::read_csv("extdata/car-msoa.csv")
 
 # Z-scores ====
 # Calculate z-score
