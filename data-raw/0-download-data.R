99f4221b4183812bdbbf3c1874491e2c378a97a7
Phil Mike Jones <philmikejones@gmail.com>
2016-07-03 15:11:43 +0100

    install to inst/extdata/

--- data-raw/0-download-data.R
+++ data-raw/0-download-data.R
@@ -1,25 +1,31 @@
 # LADs ====
-car_lad <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
+lad_car <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
                   "NM_621_1.data.csv?date=latest&",
                   "geography=1946157057...1946157404&rural_urban=0&",
                   "cell=0,1&measures=20100")
-ppr_lad <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
+downloader::download(lad_car, destfile = "inst/extdata/lad_car.csv")
+
+lad_ppr <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
                   "NM_541_1.data.csv?date=latest&",
                   "geography=1946157057...1946157404&rural_urban=0&",
                   "c_pproomhuk11=0,3,4&measures=20100")
-ten_lad <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
+downloader::download(lad_ppr, destfile = "inst/extdata/lad_ppr.csv")
+
+lad_ten <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
                   "NM_619_1.data.csv?date=latest&",
                   "geography=1946157057...1946157404&rural_urban=0&",
                   "cell=0,100&measures=20100")
-eau_lad <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
+downloader::download(lad_ten, destfile = "inst/extdata/lad_ten.csv")
+
+lad_eau <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
                   "NM_556_1.data.csv?date=latest&",
                   "geography=1946157057...1946157404&rural_urban=0&",
                   "cell=1,8&measures=20100")
+downloader::download(lad_eau, destfile = "inst/extdata/lad_eau.csv")
 
-downloader::download(car, destfile = "extdata/lad_car.csv")
-downloader::download(ppr, destfile = "extdata/lad_ppr.csv")
-downloader::download(ten, destfile = "extdata/lad_ten.csv")
-downloader::download(eau, destfile = "extdata/lad_eau.csv")
+
+
+
 
 lad <- paste0("https://census.edina.ac.uk/ukborders/easy_download/prebuilt/",
               "shape/England_lad_2011_gen.zip")
