9a47bf124aab9ee43f0e43894d29b45da7c3267d
Phil Mike Jones <philmikejones@gmail.com>
2016-07-03 15:07:42 +0100

    download lad data

--- data-raw/0-download-data.R
+++ data-raw/0-download-data.R
@@ -1,16 +1,20 @@
 # LADs ====
-car <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/NM_621_1.data.csv?",
-              "date=latest&geography=1946157057...1946157404&rural_urban=0&",
-              "cell=0,1&measures=20100")
-ppr <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/NM_541_1.data.csv?",
-              "date=latest&geography=1946157057...1946157404&rural_urban=0&",
-              "c_pproomhuk11=0,3,4&measures=20100")
-ten <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/NM_619_1.data.csv?",
-              "date=latest&geography=1946157057...1946157404&rural_urban=0&",
-              "cell=0,100&measures=20100")
-eau <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/NM_556_1.data.csv?",
-              "date=latest&geography=1946157057...1946157404&rural_urban=0&",
-              "cell=1,8&measures=20100")
+car_lad <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
+                  "NM_621_1.data.csv?date=latest&",
+                  "geography=1946157057...1946157404&rural_urban=0&",
+                  "cell=0,1&measures=20100")
+ppr_lad <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
+                  "NM_541_1.data.csv?date=latest&",
+                  "geography=1946157057...1946157404&rural_urban=0&",
+                  "c_pproomhuk11=0,3,4&measures=20100")
+ten_lad <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
+                  "NM_619_1.data.csv?date=latest&",
+                  "geography=1946157057...1946157404&rural_urban=0&",
+                  "cell=0,100&measures=20100")
+eau_lad <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
+                  "NM_556_1.data.csv?date=latest&",
+                  "geography=1946157057...1946157404&rural_urban=0&",
+                  "cell=1,8&measures=20100")
 
 downloader::download(car, destfile = "extdata/lad_car.csv")
 downloader::download(ppr, destfile = "extdata/lad_ppr.csv")
