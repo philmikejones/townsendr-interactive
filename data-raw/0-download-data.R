50cb62071fdf5ab170d9592aa9d2ff7a73752b00
Phil Mike Jones <philmikejones@gmail.com>
2016-08-15 16:37:27 +0100

    download files with utils::

--- data-raw/0-download-data.R
+++ data-raw/0-download-data.R
@@ -3,25 +3,26 @@ lad_car <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
                   "NM_621_1.data.csv?date=latest&",
                   "geography=1946157057...1946157404&rural_urban=0&",
                   "cell=0,1&measures=20100")
-downloader::download(lad_car, destfile = "inst/extdata/lad_car.csv")
+utils::download.file(lad_car, destfile = "inst/extdata/lad_car.csv")
 
 lad_ppr <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
                   "NM_541_1.data.csv?date=latest&",
                   "geography=1946157057...1946157404&rural_urban=0&",
                   "c_pproomhuk11=0,3,4&measures=20100")
-downloader::download(lad_ppr, destfile = "inst/extdata/lad_ppr.csv")
+utils::download.file(lad_ppr, destfile = "inst/extdata/lad_ppr.csv")
 
 lad_ten <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
                   "NM_619_1.data.csv?date=latest&",
                   "geography=1946157057...1946157404&rural_urban=0&",
                   "cell=0,100&measures=20100")
-downloader::download(lad_ten, destfile = "inst/extdata/lad_ten.csv")
+utils::download.file(lad_ten, destfile = "inst/extdata/lad_ten.csv")
 
 lad_eau <- paste0("https://www.nomisweb.co.uk/api/v01/dataset/",
                   "NM_556_1.data.csv?date=latest&",
                   "geography=1946157057...1946157404&rural_urban=0&",
                   "cell=1,8&measures=20100")
-downloader::download(lad_eau, destfile = "inst/extdata/lad_eau.csv")
+utils::download.file(lad_eau, destfile = "inst/extdata/lad_eau.csv")
+rm(lad_car, lad_eau, lad_ppr, lad_ten)
 
 
 # Shapefiles ====
