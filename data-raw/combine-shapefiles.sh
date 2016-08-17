83b99cf2402c73ae4d0cb96cc70c533899c6d496
Phil Mike Jones <philmikejones@gmail.com>
2016-08-17 12:18:56 +0100

    add ogr simplify to sh file

--- data-raw/combine-shapefiles.sh
+++ data-raw/combine-shapefiles.sh
@@ -3,3 +3,7 @@
 ogr2ogr -update -append -f 'ESRI Shapefile' \
   inst/extdata/lad_2011_gen.shp \
   inst/extdata/wales_lad_2011_gen.shp
+
+ogr2ogr -simplify 100 -f 'ESRI Shapefile' \
+  inst/extdata/lad_2011_simp.shp         \
+  inst/extdata/lad_2011_gen.shp
