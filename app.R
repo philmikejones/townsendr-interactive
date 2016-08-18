0866e72e46cbe6550b99060e8bc72c90df2b1540
Phil Mike Jones <philmikejones@gmail.com>
2016-08-18 10:58:09 +0100

    convert to single file shiny app

---
+++ app.R
@@ -0,0 +1,25 @@
+
+library("shiny")
+library("leaflet")
+library("magrittr")
+library("sp")
+
+ui <- fluidPage(
+
+  leafletOutput("map")
+
+)
+
+server <- function(input, output, session) {
+
+  lad_map <- readRDS("data/lad_shp.rds")
+
+  output$map  <- renderLeaflet({
+
+    leaflet()
+
+  })
+
+}
+
+shinyApp(ui, server)
