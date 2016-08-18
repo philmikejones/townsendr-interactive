e32c587df1b638e8e7ff4f27cf2c3920b5c2c455
Phil Mike Jones <philmikejones@gmail.com>
2016-08-18 11:54:28 +0100

    app works with fill and background!

--- app.R
+++ app.R
@@ -4,20 +4,33 @@ library("leaflet")
 library("magrittr")
 library("sp")
 
-ui <- fluidPage(
+ui <- fluidPage(div(class = "outer",
 
-  leafletOutput("map")
+  # Use some css bodgery for height attribute to be flexible
+  tags$style(type = "text/css", ".outer {position: fixed;
+             top: 10px; left: 10px; right: 10px; bottom: 10px;
+             overflow: hidden; padding: 0}"),
 
-)
+  leafletOutput("map", height = "100%", width = "100%")
+
+))
 
 server <- function(input, output, session) {
 
   lad_map <- readRDS("data/lad_shp.rds")
 
+  pal <- colorNumeric(
+    palette = "Blues",
+    domain  = lad_map$z
+  )
+
   output$map  <- renderLeaflet({
 
     leaflet(lad_map) %>%
-      addPolygons()
+      addPolygons(weight = 1,
+                  fillOpacity = 0.8, fillColor = ~pal(z)) %>%
+      addProviderTiles("OpenStreetMap.Mapnik",
+                       options = providerTileOptions(opacity = 0.35))
 
   })
 
